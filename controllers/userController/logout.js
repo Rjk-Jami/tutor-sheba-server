097dea18917bcd3f95d10b497e58d28dded1f321
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-28 04:43:37 +0600

    logout success

---
+++ controllers/userController/logout.js
@@ -0,0 +1,11 @@
+const logout = async (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ message: "Could not log out" });
+    }
+    res.clearCookie("connect.sid");
+    res.json({ message: "Logged out successfully" });
+  });
+};
+
+module.exports = {logout};
--- routes/authRoutes/authRoutes.js
+++ routes/authRoutes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { registration } = require('../../controllers/userController/userController');
+const { logout } = require('../../controllers/userController/logout');
 
 const authRoutes = express.Router();
 
 
 authRoutes.post("/registration", registration)
+authRoutes.post("/logout", logout)
 
 
 module.exports = authRoutes
